3d3d59927e766da41e709c028737a2602533f129
Qamar Waqar <qamogle@gmail.com>
2017-03-31 15:32:50 +0500

    Out Variables Wild Card and Parameter Declaration
    You can call an out Variable Function with a wild card and when calling
    out Var Function, you can declare Var in the Function Parameter List

--- CSharp7.0/Program.cs
+++ CSharp7.0/Program.cs
@@ -10,6 +10,34 @@ namespace CSharp7._0
     {
         static void Main(string[] args)
         {
+            #region 1st Feature: out Variables Wild card and Parameter Declaration
+            // Old way of calling an out variable Function //
+            int a, b = 0;
+            OutVarFunction(out a, out b);
+            // This is new in C# 7.0 //
+            // Now you can call an out Variable Function and //
+            // Declare the variable right when you are calling the function //
+            // Variable have to be modified before the function returns //
+            OutVarFunction(out int c, out int d);
+            // Keep in mind that c,d,e,f are now local variables of thie function //
+            OutVarFunction(out var e, out var f);
+
+            // the code underneath uses sterik as second parameter //
+            // which means that I only care about first parameter, ignore the second one //
+            // because the inner function will modify both variables any ways //
+            // so I am ignoring the second one, what ever type or value it has before hand //
+            // this is not yet supported but it's in focus //
+
+            // OutVarFunction(out var g, out *);
+            #endregion
+
         }
+        #region 1st Feature: out Variables Wild card and Parameter Declaration
+        static void OutVarFunction(out int y, out int x)
+        {
+            x = 0; y = 0;
+            Console.WriteLine("The x is: " + x + "\nThe y is: " + y);
+        }
+        #endregion
     }
 }
